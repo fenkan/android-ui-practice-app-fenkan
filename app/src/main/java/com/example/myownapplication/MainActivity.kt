1eaedd363c74d9f277a63ae36f885f856dea2a72
fenkan <fenkan-97@hotmail.com>
2022-04-01 04:04:41 +0200

    project activity switch

--- app/src/main/java/com/example/myownapplication/MainActivity.kt
+++ app/src/main/java/com/example/myownapplication/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.myownapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.myownapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    //defining the variables
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity:: class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
+
+
+
+
 }
